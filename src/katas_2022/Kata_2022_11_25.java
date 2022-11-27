3d6a564b503c902277159b10aa8b007852157b93
Dario <dariomsn@yahoo.com.ar>
2022-11-27 09:55:41 -0300

    commit

---
+++ src/katas_2022/Kata_2022_11_25.java
@@ -0,0 +1,32 @@
+package katas_2022;
+
+public class Kata_2022_11_25 {
+
+	public static void main(String[] args) {
+		
+		int posicionPar = 0;
+		int posicionImpar = 19;
+		int[] numeros = new int[20];
+		
+		for(int i=0; i<20; i++) {
+			
+			double numeroD = Math.random() * 100;
+			int numero = (int) numeroD;
+			
+			if(numero % 2 == 0) {
+				numeros[posicionPar] = (int) numero;
+				posicionPar++;
+			}else {
+				numeros[posicionImpar] = (int) numero;
+				posicionImpar--;
+			}
+		}
+		int posicion = 1;
+		for (int j : numeros) {
+			System.out.println("posicion " + posicion + " valor " + j);
+			posicion++;
+		}
+		
+	}
+
+}
